c3c79151c1a74edccc560a2183e1ddef17bb5ceb
Massipssa Kerrache <kerrache.massipssa@gmail.com>
2025-09-07 18:40:25 +0200

    Deploy website - based on 7cab192883bf8a385373f0b8e9fdb7ec694803f2

--- cloud/azure/deployment/test.sh
+++
@@ -1,5 +0,0 @@
-RESOURCE_GROUP="test-rg"
-az  deployment group create -g $RESOURCE_GROUP \
-    --template-file output.json > result_output.json \
-    --paramerters @output.paramerters-dev.json \
-    --paramerters parameterName=value # override parameter
